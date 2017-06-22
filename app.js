37e4ba05feb37fc65e6e5dd8bd4b776480950dc0
ikemura <banbara.work@gmail.com>
2017-06-22 10:03:24 +0900

    算出プロパティでimage_url属性を追加、 results配列を4つにまとめる

--- app.js
+++ app.js
@@ -21,5 +21,23 @@ const vm = new Vue({
         this.results = response.data.results;
       }).catch(error => { console.log(error); })
     }
+  },
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      // Add image_url attribute
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      })
+
+      // Put Array into Chunks
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i = 0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i, i + chunk);
+      }
+      return chunkedArray;
+    }
   }
 });
